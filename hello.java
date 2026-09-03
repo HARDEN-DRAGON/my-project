e5fe3cfbc7b2b8d5bb54eac87a066dbe07a22c15
HARDEN-DRAGON <jharivenkatesh18@gmail.com>
2026-09-03 11:29:32 +0530

    Add additional print statement for help message

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
   public static void main(String[]args){
     System.out.println("Hello i am in git repo pls save me");
+    System.out.println("i am under the water pls help me");
   }
 }
