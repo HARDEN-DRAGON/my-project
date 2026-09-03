bf72cc05170ce2b0bc0be25a0c9286865ae357cd
HARDEN-DRAGON <jharivenkatesh18@gmail.com>
2026-09-03 11:33:50 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class hello{
+public class Hello{
   public static void main(String[]args){
     System.out.println("Hello i am in git repo pls save me");
     System.out.println("i am under the water pls help me");
